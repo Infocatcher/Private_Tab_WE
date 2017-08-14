d7dfc6e1c04cfc17fa976555c81f9ad70ca3c31c
Infocatcher <infocatcher.dev.null@gmail.com>
2017-08-14 21:50:09 +0300

    Add page context menu item (dummy) + basic localization

---
+++ background.js
@@ -0,0 +1,32 @@
+const LOG_PREFIX = "[Private Tab WE] ";
+
+function onCreated() {
+	var err = browser.runtime.lastError;
+	if(err)
+		_err("Error: " + err);
+	else
+		_log("Item created successfully");
+}
+
+browser.contextMenus.create({
+	id: "pt-openInTab",
+	title: browser.i18n.getMessage("openInNewPrivateTab"),
+	contexts: ["link"]
+}, onCreated);
+
+browser.contextMenus.onClicked.addListener(function(info, tab) {
+	_log("onClicked: " + info.menuItemId);
+});
+
+function ts() {
+	var d = new Date();
+	var ms = d.getMilliseconds();
+	return d.toTimeString().replace(/^.*\d+:(\d+:\d+).*$/, "$1") + ":" + "000".substr(("" + ms).length) + ms + " ";
+}
+function _log(s) {
+	//if(_dbg)
+	console.log(LOG_PREFIX + ts() + s);
+}
+function _err(s) {
+	console.error(LOG_PREFIX + ts() + s);
+}
---
+++ make.bat
@@ -0,0 +1,67 @@
+@echo off
+set _out=private_tab_we-latest.xpi
+set _out_tmp=%~d0\~%_out%.tmp
+set _7zip="%COMMANDER_PATH%\arch\7-Zip-4.65\7z.exe"
+set _winRar="%COMMANDER_PATH%\arch\WinRAR\WinRAR.exe"
+title Make %_out%
+
+if not exist %_7zip% set _7zip="%ProgramFiles%\7-Zip\7z.exe"
+if not exist %_winRar% set _winRar="%ProgramFiles%\WinRAR\WinRAR.exe"
+
+if not exist %_7zip% (
+	title Error - Make %_out%
+	echo 7-Zip not found!
+	if not exist %_winRar% (
+		echo WinRAR not found!
+		pause
+		exit /b
+	)
+)
+
+cd /d "%~dp0"
+
+if exist %_out_tmp% (
+	title Error - Make %_out%
+	echo =============================================
+	echo Something went wrong, please remove or rename
+	echo %_out_tmp%
+	echo =============================================
+	pause
+	exit /b
+)
+
+:: Test for write access
+type nul > %_out_tmp%
+if not exist %_out_tmp% (
+	echo =^> %_out_tmp% isn't writable
+	echo ==^> will use %temp%
+	set _out_tmp="%temp%\~%_out%.tmp"
+) else (
+	del %_out_tmp%
+)
+
+set _files=_locales *.js *.json *.png
+if exist %_7zip% (
+	echo =^> %_7zip%
+	%_7zip% a -tzip -mx9 -mfb=258 -mpass=15 -- %_out_tmp% %_files%
+) else (
+	echo =^> %_winRar%
+	%_winRar% a -afzip -m5 -r -- %_out_tmp% %_files%
+)
+
+if not exist %_out_tmp% echo Error: %_out_tmp% not found! & pause & exit /b
+
+if not exist %_out% goto skipCompare
+fc /l /lb2 /t %_out% %_out_tmp%
+if %ErrorLevel% == 0 (
+	del %_out_tmp%
+	echo ===============
+	echo ==^> No changes!
+	echo ===============
+	if not "%1" == "nodelay" ping -n 3 127.0.0.1 > nul
+	exit /b
+)
+:skipCompare
+
+echo ==^> Changed, update...
+move /y %_out_tmp% %_out%
