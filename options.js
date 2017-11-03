002424e7ea80319cfaea21ea2a44da711bf4bc84
Infocatcher <infocatcher.dev.null@gmail.com>
2017-11-03 21:28:11 +0300

    Simplify: remove unused features from options code

--- options.js
+++ options.js
@@ -4,10 +4,6 @@ var prefs = { // Dafaults
 };
 function loadOptions() {
 	browser.storage.local.get().then(function(o) {
-		browser.storage.onChanged.addListener(function(changes, area) {
-			if(area == "local") for(var key in changes)
-				loadOption(key, changes[key].newValue);
-		});
 		Object.assign(prefs, o);
 		for(var id in prefs)
 			loadOption(id, prefs[id]);
@@ -15,28 +11,15 @@ function loadOptions() {
 }
 function loadOption(id, val) {
 	var node = document.getElementById(id);
-	node && setValue(node, val);
+	node && (node.checked = val);
 }
 function saveOption(e) {
 	var node = e.target;
 	if(!(node.id in prefs))
 		return;
 	browser.storage.local.set({
-		[node.id]: getValue(node)
+		[node.id]: node.checked
 	});
-}
-function getValue(node) {
-	return node.localName == "select" || node.type == "number"
-		? +node.value
-		: node.type == "checkbox"
-			? node.checked
-			: node.value;
-}
-function setValue(node, val) {
-	if(node.type == "checkbox")
-		node.checked = val;
-	else
-		node.value = val;
 }
 document.addEventListener("DOMContentLoaded", loadOptions, true);
 document.addEventListener("input", saveOption, false);
